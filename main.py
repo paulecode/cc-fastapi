c677a021f466c3fdd9e6e120025d4d4c587cc77f
Paul Ebert <paul.ebert@code.berlin>
2024-09-11 21:18:33 +0200

    clean main.py up

--- main.py
+++ main.py
@@ -1,40 +1,30 @@
-import mido
-import pickle
-from sklearn.ensemble import RandomForestClassifier
-from fastapi import BackgroundTasks, FastAPI, UploadFile
+from helper.chord_extract import chord_extract
+from helper.midi_processor import midi_preprocess
+
+from fastapi import FastAPI, UploadFile
+
+from helper.note_name import get_note_name
 
 app = FastAPI()
 
-model_filename = 'random_forest_model.pkl'
-with open(model_filename, 'rb') as file:
-    model = pickle.load(file)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
 
 
-@app.get("/express")
-def read_express():
-    print("Test")
-    return {"message": "Live v3"}
-
 def preprocessFile(file: UploadFile):
     print(file.content_type)
-    note_count = 0
-    midi_file = mido.MidiFile(file=file.file)
-    for msg in midi_file:
-        if msg.type == 'note_on':
-            note_count += 1
-    prediction = model.predict([[note_count]])
 
-    print(prediction)
-    return prediction
+    note_df, meta_df = midi_preprocess(file)
+    print("Done processing")
+    note_df = get_note_name(note_df)
+    chord_df = chord_extract(note_df)
+
+    return chord_df
+
 
 @app.post("/predict")
-async def predict(file: UploadFile, background_tasks: BackgroundTasks):
-    if not file:
-        raise Exception("File missing")
-
-    background_tasks.add_task(preprocessFile, file)
-    return {"message": file.content_type}
+async def predict(file: UploadFile):
+    chord_df = preprocessFile(file)
+    return {"chords": chord_df.to_dict()}
